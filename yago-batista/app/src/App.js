cb85ca3dd802826dfaedb308a92989e93a80b375
yago.silva <yagobatistasilva@gmail.com>
2021-06-16 10:00:24 -0300

    correct text and deploy

--- yago-batista/app/src/App.js
+++ yago-batista/app/src/App.js
@@ -1,7 +1,7 @@
-import { Row, Card, Col, Badge, Input } from 'antd';
-import {useState} from 'react'
+import { Row, Card, Badge, Input } from 'antd';
+import { useState } from 'react'
 import 'antd/dist/antd.css';
-import {ModalNewData} from './ModalNewData'
+import { ModalNewData } from './ModalNewData'
 import './App.css'
 
 export const Status = {
@@ -39,7 +39,7 @@ let defaultData = [
     props: ["vacinacao", "Nao tem atendimento para covid"]
   },
   {
-    img: "http://culturaniteroi.com.br/blog/imagens/488_capa.jpg",
+    img: "https://www.saudebusiness.com/sites/saudebusiness.com/files/styles/article_featured_retina/public/uploads/2020/06/hospital-icarai%CC%81-480x335.jpg?itok=4nq3_uQO",
     name: "Icaraí",
     type: Types.HOSPITAL,
     status: Status.VAZIO,
@@ -60,11 +60,10 @@ let defaultData = [
     type: Types.HOSPITAL,
     status: Status.CHEIO,
     time: "3h",
-    props: [ "Atendimento para covid"]
+    props: ["Atendimento para covid"]
   },
 
 ]
-defaultData = [...defaultData]
 
 
 
@@ -72,8 +71,8 @@ function App() {
   const [data, setData] = useState(defaultData);
   const [result, setResult] = useState(defaultData);
 
-  const handleSearch = ({target: {value}}) => {
-    setResult(data?.filter(({name}) => {
+  const handleSearch = ({ target: { value } }) => {
+    setResult(data?.filter(({ name }) => {
       return name.toLowerCase().indexOf(value.toLowerCase()) > -1
     }));
   };
@@ -81,20 +80,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Unilasalle - 2021.1 - CSCW-AV2</h1>
+        <h1>Unilasalle - 2021.1 - CSCW-AV2 - Yago Batista da Silva - Matrícula 0050017826</h1>
         <h2> O melhor posto</h2>
         <p>
-        O objetivo do trabalho é criar uma aplicacao de crowdsourcing em que os usuarios compartilhem informacoes em tempo real sobre a comida, como postos que estao realisando atendimento para covid-19, informar quando o posto está lotado
+          O objetivo do trabalho é criar uma aplicação de crowdsourcing em que os usuários compartilhem e consultem informações em tempo real sobre a covid-19, como por exemplos postos que estão realizando atendimento ou vacinação, como o posto/hospital estava na sua última consulta, tempo médio de consulta e etc.
         </p>
       </header>
       <main>
-
         <>
-
-          <Input 
-          placeHolder="Busca"
-          onChange={handleSearch}
-          style={{width: "50%", margin: 10}}/>
+          <Input
+            placeHolder="Busca"
+            onChange={handleSearch}
+            style={{ width: "50%", margin: 10 }} />
           <ModalNewData data={data} setData={setData}></ModalNewData>
           <Row>
             {result?.map(({ img, type, name, time, props, status: { label, badge } }) => (
